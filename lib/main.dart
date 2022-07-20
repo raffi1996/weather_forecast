6da122d03b459715d98d9fba674995e52d94a767
raffi1996 <Dilleros300405@gmail.com>
2022-07-20 20:20:32 +0400

    added next api call

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:weather_forecast/http/repositories/weather_repository.dart';
 
 import 'app.dart';
 
 Future<void> main () async {
+ // final x = await WeatherRepository.getCurrentWeather();
 
+  //log(x.toString());
   runApp(const MyApp());
 }
